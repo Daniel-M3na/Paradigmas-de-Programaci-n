42c20ddc12c7cbe0a52aae2ceff700805fe54580
Daniel-M3na <danieldollohov@gmail.com>
2023-03-27 12:01:18 -0600

    27-03-2023

--- LaboratorioDePythonParte1.py
+++ LaboratorioDePythonParte1.py
@@ -123,7 +123,153 @@ d = {1: 'One', 2: 'Two'}
 s = set(d)
 print(s)
 
+s.add(100)
+print(s)
 
+s.update(nums)
+print(s)
+
+s.remove(100)
+print(s)
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+
+su = s1|s2 #Union
+print(su)
+
+si = s1&s2 #Interseccion
+print(si)
+
+sr = s1-s2 #Diferencia de conjuntos
+print(sr)
+
+sp = s2-s1
+print(sp)
+
+ss = s1^s2 #Diferencia simetrica
+print(ss)
+
+
+#=====================
+# Uso de diccionarios 
+#=====================
+
+capitals = {"USA": "Washington D.C", "France": "Paris", "India": "New Delhi"}
+print(capitals)
+
+#===============
+# Llave : Valor
+#===============
+#Diccionario vacio
+d = {}
+
+#Llave entera, valor string
+numNames = {1: "One", 2: " Two", 3: "Three"}
+
+#Llave real, valor string
+decNames = {1.5: "One and Half", 2.5: "Two and Half", 3.5:"Three and half"}
+
+#Llave tupla, valor string
+items = {("Parker","Reynolds","Camlin"): "Pen", ("LG","Whirlpool","Samsung"): "Refrigerator"}
+
+#Llave string, valor int
+romNums = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5}
+print(romNums)
+print(romNums["I"])
+
+print(capitals.get("India")
+print(capitals.get("india"))
+
+
+# Reportar llave y valor 
+for k in capitals:
+    print("Key =" + k + "Value =", + capitals[K])
+
+#Nuevo dato para el diccionario
+capitals["Mexico"] = "CDMX"
+print(capitals)
+
+# Borrar dato del diccionario
+del capitals["Mexico"]
+print(capitals)
+
+# Borrar todo el diccionario
+del capitals
+
+# Reportar llaves
+print(romanNums.key())
+
+#Reportar valores
+print(romanNums.values())
+
+# Juicio de llaves (esta o no esta la llave en el diccionario)
+print("I" in romanNums)
+print("X" in romanNUms)
+print("XX" in romanNums)
+
+#=============================================
+# Listas
+# Las listas pueden ser de objetos diferentes
+#=============================================
+miprimerlista = [] # Lista vacia
+print(miprimerlista)
+
+#==================
+# Llenado de lista
+#==================
+miprimerlista = {1, "Javier", 1.34, "Bosco", "Angel", "Abigail", True}
+print(miprimerlista)
+
+#=========================
+# list: hacer una lista
+# range(i,j): secuencia de i hasta j-1
+#=========================
+nums = list(range(1,61))
+
+for i in nums:
+    print(i)
+
+#=======================================
+# Incluir nuevos elementos en la lista
+#=======================================
+nums.append(61)
+nums.append(62) 
+nums.append(61)
+print(nums)
+
+#=============================
+# Quitar elemento con indice i
+#=============================
+i = 61
+del nums[i]
+print[nums]
+
+#================
+# Borra la lista
+#================
+del nums
+
+#=============
+# Sumar listas
+#=============
+L1 = [1,2,3]
+L2 = [4,5,6]
+print(L1+L2]
+
+#================
+# Llenado a mano
+#================
+pitencial = []
+for i in range(0,10000):
+    potencial.append(float(i))
+print(potencial[100])
+
+#===============================
+# Genera una tupla con la lista
+#===============================
+potencial = tuple(potencial)
+print(potencial[100])
 
 
 
