ecba842a7a728c6c745cba4c519da5a884f4d702
Daniel-M3na <danieldollohov@gmail.com>
2023-04-03 13:01:47 -0600

    3-April-2023

--- LaboratorioDePythonParte1.py
+++ LaboratorioDePythonParte1.py
@@ -48,13 +48,13 @@ print("Hola"); print("tu!!") #Se considera mala práctica
 #se puede escribir en varios renglones
 #==============================================
 
-list = [1, 2, 3, 4, 
+lista = [1, 2, 3, 4, 
         5, 6, 7, 8,
         9, 10, 11, 12]
 
 matriz = [ [1,2,3,4],[5,6,7,8],[9,10,11,12] ]
 
-print(list)
+print(lista)
 print(matriz)
 
 #===============================================================
@@ -64,7 +64,7 @@ print(matriz)
 if 10>5:
     print ("diez es mayor que cinco")
     print("otra indentación")
-for i in list:
+for i in lista:
     print(i)
     print("ok")
 if 10>5:
@@ -225,30 +225,30 @@ print(miprimerlista)
 # list: hacer una lista
 # range(i,j): secuencia de i hasta j-1
 #=========================
-nums = list(range(1,61))
+Nums = list(range(1,61))
 
-for i in nums:
+for i in Nums:
     print(i)
 
 #=======================================
 # Incluir nuevos elementos en la lista
 #=======================================
-nums.append(61)
-nums.append(62) 
-nums.append(61)
-print(nums)
+Nums.append(61)
+Nums.append(62) 
+Nums.append(61)
+print(Nums)
 
 #=============================
 # Quitar elemento con indice i
 #=============================
 i = 61
-del nums[i]
-print[nums]
+del Nums[i]
+print(Nums)
 
 #================
 # Borra la lista
 #================
-del nums
+del Nums
 
 #=============
 # Sumar listas
@@ -260,7 +260,7 @@ print(L1+L2)
 #================
 # Llenado a mano
 #================
-pitencial = []
+potencial = []
 for i in range(0,10000):
     potencial.append(float(i))
 print(potencial[100])
@@ -403,3 +403,154 @@ salida = saludo()
 #==================
 # Esto no funciona
 #=================
+print(salida)
+
+#=======================
+# Mostrar documentacion 
+#=======================
+# help()
+
+#======================
+# Función con argumento
+#======================
+def salu2(nombre):
+    """Esta función te saluda por tu nombre"""
+    print("Qué onda ese",nombre,"!")
+
+salu2("Julián")
+salu2("Ángel")
+
+#==========================================
+# Ahorrar trabajo del intérprete
+# nombre: str la variable nombre es un str
+#==========================================
+def saludos(nombre:str):
+    """Esta función te saluda por tu nombre estrictamente"""
+    print("¡Que onda ese",nombre ,"!")
+saludos("Julián")
+a = 123
+print(type(a))
+saludos(a)
+
+#===============================
+# FUnción con muchos argumentos
+#===============================
+def saludos_multiples(nombre1,nombre2,nombre3):
+    """Esta funcion saluda a 3 personas al mismo tiempo"""
+    print("Hola",nombre1,",",nombre2,",",nombre3)
+saludos_multiples("Hugo","Paco","Luis")
+
+#===========================================
+# Funcion con cuaqluier numero de argumentos
+#===========================================
+def muchos_saludos(*nombres):
+    """Esta funcion saluda a todos los que quieras"""
+    i = 0
+    #=================================
+    # end= es para ponerlo de corrido
+    #=================================
+    print("Hola",end="")
+    while len(nombres)>i:
+        #Ultimo nombre
+        if (i== len(nombres)-1):
+            print(nombres[i])
+        else:
+            #Cualquier otro nombre 
+            print(nombres[i],end= "")
+        i+=1
+
+muchos_saludos("Bosco","Angel","David","Tamara","Mili","Edwin","Luis","Abigail")
+
+def greet(firstname, lastname):
+    print("Hello",firstname, lastname)
+
+#============================================
+# Llamar funcion con argumentos en desorden
+#============================================
+greet(lastname="Jobs", firstname="Steve") # Se pueden especificar las variables en desorden
+
+#=====================================
+# Funcion con argumentos escondidos **
+#======================================
+def greet(**person):
+    #===================================================
+    # persona tiene características firstname y lastname
+    #===================================================
+    print("Hello",person["firstname"],person["lastname"])
+
+greet(firstname="Steve", lastname="Jobs")
+greet(lastname="Jobs", firstname="Steve")
+greet(firstname="Bill",lastname="Gates", age=55) # Se pueden pasar mas parametros de los necesarios
+
+
+#======================
+# Funcion con resultado
+#======================
+
+def suma(a,b):
+    return a+b
+
+#==================================
+# Programacion funcional
+# Se pueden funciones en funciones
+#==================================
+total = suma(5,suma(10,20))
+print(total)
+
+#================================================
+# Calculo lambda
+# nombre de la funcion = lambda variable: funcion
+#================================================
+x_al_cuadrado = lambda x:  x*x
+a1 = x_al_cuadrado(5)
+print(a1)
+
+#===========================
+# Lambda de varias variables
+#===========================
+suma = lambda x1, x2, x3: x1+x2+x3
+print(suma(99,98,97))
+
+sumas = lambda *x: x[0]+x[1]+x[2]+x[3]
+print(sumas(100,200,300,400))
+
+#=========================================
+# Uso de alguna funcion anonima
+# El argumento va afuera entre parentesis
+#=========================================
+print((lambda x: x*x)(6)) #Funcion anonima
+
+#=============================
+# Funcion con variable global
+# EVITE EL EXCESO !!!!!
+#=============================
+name = "Steve"
+def greet():
+    global name # Para utilizar una varibale global (que viene del bloque)
+    name = "Bill"
+    print("Hello",name)
+
+greet()
+
+
+
+#================================
+# Algoritmo 1
+#================================
+# Serie exponencial
+# Factorizacion de x
+# Negativos con funcion inversa
+#=================================
+n = 200
+x = -100.0
+flag = False
+if x<0:
+    flag = True
+    x = -x
+S = 1.0
+for i in range(n,0,-1):
+    s *= x/float(i)
+    s += 1.0
+if flag == True:
+    s = 1/s
+print(s)
