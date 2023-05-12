01ca599ffc290ac98605b38ac495d8eb6f96bb42
Daniel-M3na <danieldollohov@gmail.com>
2023-05-12 13:37:22 -0600

    May 12th

--- curva_zspline.py
+++ curva_zspline.py
@@ -1,43 +1,45 @@
-#========================================
-# Curvas Z-splines en n dimensiones
-#========================================
+#===================================
+# Curvas Z-spline en n dimensiones
+#===================================
 # Julián Sagredo ESFM IPN 2023
-#========================================
+#===================================
 
 import numpy as np
 from Curva import Curva,zspline
 import matplotlib.pyplot as plt
 import math
 
-#===================
-# Conjunto de putos
+#====================
+# Conjunto de puntos 
 #===================
 #Número de puntos
 nump:np.int32 = 8
-#Dimension
+#Dimensión
 dim:np.int32 = 2
 #Memoria para puntos
 puntos = np.zeros(dim*nump,dtype=np.float64)
-#Parametrizacion
+#Parametrización 
 X = np.linspace(0.0,2*np.pi,nump+1)
-#Coordenada X
+#Coordenada x
 puntos[0:nump] = np.cos(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
 #Coordenada y
 puntos[nump:2*nump] = np.sin(X[0:nump]) + 0.0*np.sin(2*X[0:nump])
 
-#==============================================================
+#================================================================
 # Curva Z-spline de n puntos zspline(p,d,n,m)
-# p: puntos, d: dimension, n:segmentos de curva, m: continuidad
-#==============================================================
+# p: puntos, d: dimensión, n: segmentos de curva, m: continuidad
+#================================================================
 n:np.int32 = 1000
-#x1,y1 = zspline(puntos,dim,n,2)
-#x2,y2 = zspline(puntos,dim,n,1)
+x1,y1 = zspline(puntos,dim,n,2)
+x2,y2 = zspline(puntos,dim,n,1)
 x3,y3 = zspline(puntos,dim,n,0)
-plt.plot(x3,y3,lw=2,color="orange")
-#plt.plot(x2,y2,lw=2,color="red")
-#plt.plot(x1,y1,lw=2,color="blue")
-plt.scatter(puntos[0:nump],puntos[nump:2*nump],marker='o',color="black")
+plt.plot(x3,y3,lw =2,color="orange")
+plt.plot(x2,y2,lw=2,color="red")
+plt.plot(x1,y1,lw=2,color="blue")
+plt.scatter(puntos[0:nump], puntos[nump:2*nump], marker="o", color="black")
 plt.xlabel("Coordenada x")
-plt.ylabe("Coordenada Y")
+plt.ylabel("Coordenada Y")
 plt.title("Curva cerrada Z-spline en 2D")
 plt.show()
+
+
