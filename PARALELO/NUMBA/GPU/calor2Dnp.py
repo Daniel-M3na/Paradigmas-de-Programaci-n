db1ad38e0b90035c2d3e379423020f4530c76eb4
Daniel-M3na <danieldollohov@gmail.com>
2023-06-06 01:28:21 -0600

    Nuevo

--- PARALELO/NUMBA/GPU/calor2Dnp.py
+++ PARALELO/NUMBA/GPU/calor2Dnp.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import time
+from numba import jit
 
-#-------------------------------------------------------
+#----------------------------------------------------
 # Número de celdas
 n = np.array([512,512])
 # Tamaño del dominio (menor que uno)
@@ -12,22 +13,23 @@ L = np.array([1.0,1.0])
 # Constante de difusión
 k = 0.2
 # Pasos de tiempo
-pasos = 100
-#--------------------------------------------------------
+pasos = 1000
+#----------------------------------------------------
 
-# Tamaño de celdas
+#Tamaño de las celdas
 dx = L/n
 udx2 = 1.0/(dx*dx)
-#Paso de tiempo
+# Paso de tiempo
 dt = 0.25*(min(dx[0],dx[1])**2)/k
-print("dt = ", dt)
+print("dt = ",dt)
 # Total de celdas
 nt = n[0]*n[1]
-print("Celdas = ",nt)
+print("Celdas = ", nt)
 # Llenar la solución con ceros
-u = np.zeros(nt, dtype = np.float64) # arreglo de lectura
-un = np.zeros(nt, dtype = np.float64) # arreglo de escritura
+u = np.zeros(nt,dtype = np.float64) # Arreglo de lectura
+un = np.zeros(nt,dtype = np.float64) # Arreglo de escritura
 
+@jit(nopython=True)
 def evolucion(u,n,udx2,dt,i,k):
     jp1 = i + n[0]
     jm1 = i - n[0]
@@ -35,7 +37,8 @@ def evolucion(u,n,udx2,dt,i,k):
     unueva = u[i] + dt*k*laplaciano
     return unueva
 
-def solucion(u, un, udx2, dt, n, k):
+@jit(nopython=True)
+def solucion(u,un, udx2, dt, n, k):
     for jj in range(1,n[1]-1):
         for ii in range(1,n[0]-1):
             i = ii + n[0]*jj
@@ -44,16 +47,15 @@ def solucion(u, un, udx2, dt, n, k):
                 unueva = 1.0
             un[i] = unueva
 
+x,y = np.meshgrid(np.arange(0,L[0],dx[0]),np.arange(0,L[1],dx[1]))
+ax = plt.axes(projection='3d')
 start = time.time()
-for t in range(1,pasos+1):
+for t in range(1, pasos+1):
     solucion(u,un,udx2,dt,n,k)
     u = un
-    if t%100 == 0: print("Iteración = ", t)
+    if t % 100 == 0: print("Iteración = ", t)
 end = time.time()
-print("Tardó: ", end-start,"s")
-
-u = np.reshape(u,(n[0],n[1]))
-x,y = np.meshgrid(np.arange(0,L[0],dx[0]),np.arange(0,L[1],dx[1]))
-ax = plt.axes(projection = '3d')
-ax.plot_surface(x,y,u,cmap = cm.hsv)
+print("Tardó = ",end-start, "s")
+up = np.reshape(u,(n[0],n[1]))
+ax.plot_surface(x,y,up,cmap=cm.hsv)
 plt.show()
